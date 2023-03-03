2c25c5c04c3753fe04f055cd3b76935965996edd
Fernando HBS <fernandobailao@gmail.com>
2023-03-02 22:49:17 -0300

    Delete server directory

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import connectToDatabase from "./database.js";
-import express from "express";
-
-connectToDatabase();
-const app = express();
-
-app.use(express.json());
